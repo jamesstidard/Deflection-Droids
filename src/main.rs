95b19577b2052463055dce2083943712aaa650e4
James Stidard <james@stidard.com>
2019-05-11 20:23:17 +0100

    transaparent textures

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate amethyst;
 use amethyst::{
     prelude::*,
-    renderer::{DisplayConfig, DrawFlat2D, Pipeline, PosNormTex, RenderBundle, Stage},
+    renderer::{DisplayConfig, DrawFlat2D, Pipeline, PosNormTex, RenderBundle, Stage, ColorMask, ALPHA},
     utils::application_root_dir,
     core::transform::TransformBundle,
 };
@@ -24,7 +24,7 @@ fn main() -> amethyst::Result<()> {
     let pipe = Pipeline::build().with_stage(
         Stage::with_backbuffer()
             .clear_target([0.00196, 0.23726, 0.21765, 1.0], 1.0)
-            .with_pass(DrawFlat2D::new()),
+            .with_pass(DrawFlat2D::new().with_transparency(ColorMask::all(), ALPHA, None)),
     );
     let render_bundle = RenderBundle::new(pipe, Some(config))
         .with_sprite_sheet_processor();
