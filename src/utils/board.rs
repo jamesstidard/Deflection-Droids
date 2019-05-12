e9bba25fe2a5f4d9f13e6d2c172a37aea0a2f577
James Stidard <james@stidard.com>
2019-05-12 01:38:13 +0100

    walls

--- src/utils/board.rs
+++ src/utils/board.rs
@@ -13,6 +13,59 @@ pub const Y_TILES_COUNT: i32 = 16;
 pub const BOARD_WIDTH: f32 = TILE_WIDTH * (X_TILES_COUNT as f32);
 pub const BOARD_HEIGHT: f32 = TILE_HEIGHT * (Y_TILES_COUNT as f32);
 
+const WALLS: [[f32; 2]; 50] = [
+    [6.0, 0.5],
+    [13.0, 0.5],
+    [3.5, 1.0],
+    [4.0, 1.5],
+    [11.0, 1.5],
+    [0.5, 2.0],
+    [10.5, 2.0],
+    [8.0, 2.5],
+    [8.5, 3.0],
+    [7.0, 3.5],
+    [6.5, 4.0],
+    [15.5, 4.0],
+    [1.0, 4.5],
+    [12.0, 4.5],
+    [1.5, 5.0],
+    [5.5, 5.0],
+    [9.5, 5.0],
+    [12.5, 5.0],
+    [5.0, 5.5],
+    [10.0, 5.5],
+    [14.5, 6.0],
+    [14.0, 6.5],
+    [7.5, 7.0],
+    [8.5, 7.0],
+    [7.0, 7.5],
+    [9.0, 7.5],
+    [7.0, 8.5],
+    [9.0, 8.5],
+    [7.5, 9.0],
+    [8.5, 9.0],
+    [0.5, 9.0],
+    [5.0, 9.5],
+    [4.5, 10.0],
+    [15.5, 10.0],
+    [12.0, 10.5],
+    [1.5, 11.0],
+    [10.5, 11.0],
+    [12.5, 11.0],
+    [1.0, 11.5],
+    [10.0, 11.5],
+    [6.5, 12.0],
+    [7.0, 12.5],
+    [14.0, 12.5],
+    [11.5, 13.0],
+    [13.5, 13.0],
+    [12.0, 13.5],
+    [3.0, 14.5],
+    [3.5, 15.0],
+    [2.0, 15.5],
+    [12.0, 15.5],
+];
+
 
 pub fn initialise(world: &mut World, sprite_sheet_handle: SpriteSheetHandle) {
     let tile_render = SpriteRender {
@@ -40,7 +93,12 @@ pub fn initialise(world: &mut World, sprite_sheet_handle: SpriteSheetHandle) {
                 .build();
 
             // left wall
-            if x_tile == 0 {
+            let left_edge = x_tile == 0;
+            let left_wall = WALLS
+                .iter()
+                .find(|&&w| w == [(x_tile as f32)+0.0, (y_tile as f32)+0.5])
+                .is_some();
+            if left_edge || left_wall {
                 let mut wall_transform = Transform::default();
                 wall_transform.roll_local(0f32.to_radians());
                 world
@@ -53,7 +111,12 @@ pub fn initialise(world: &mut World, sprite_sheet_handle: SpriteSheetHandle) {
             }
 
             // top wall
-            if y_tile == Y_TILES_COUNT-1 {
+            let top_edge = y_tile == Y_TILES_COUNT-1;
+            let top_wall = WALLS
+                .iter()
+                .find(|&&w| w == [(x_tile as f32)+0.5, (y_tile as f32)+1.0])
+                .is_some();
+            if top_edge || top_wall {
                 let mut wall_transform = Transform::default();
                 wall_transform.roll_local(90f32.to_radians());
                 world
@@ -66,7 +129,12 @@ pub fn initialise(world: &mut World, sprite_sheet_handle: SpriteSheetHandle) {
             }
 
             // right wall
-            if x_tile == X_TILES_COUNT-1 {
+            let right_edge = x_tile == X_TILES_COUNT-1;
+            let right_wall = WALLS
+                .iter()
+                .find(|&&w| w == [(x_tile as f32)+1.0, (y_tile as f32)+0.5])
+                .is_some();
+            if right_edge || right_wall {
                 let mut wall_transform = Transform::default();
                 wall_transform.roll_local(180f32.to_radians());
                 world
@@ -79,7 +147,12 @@ pub fn initialise(world: &mut World, sprite_sheet_handle: SpriteSheetHandle) {
             }
 
             // bottom wall
-            if y_tile == 0 {
+            let bottom_edge = y_tile == 0;
+            let bottom_wall = WALLS
+                .iter()
+                .find(|&&w| w == [(x_tile as f32)+0.5, (y_tile as f32)+0.0])
+                .is_some();
+            if bottom_edge || bottom_wall {
                 let mut wall_transform = Transform::default();
                 wall_transform.roll_local(270f32.to_radians());
                 world
