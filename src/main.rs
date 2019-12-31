52d2ec79ed779a5271c8a759271dc06c7e7551ef
James Stidard <james@stidard.com>
2019-12-31 18:49:37 +0000

    moved config

--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,9 @@ fn main() -> amethyst::Result<()> {
 
     let app_root = application_root_dir()?;
     let resources = app_root.join("resources");
-    let display_config = resources.join("display_config.ron");
-    let binding_path = resources.join("bindings_config.ron");
+    let config = app_root.join("config");
+    let display_config = config.join("display.ron");
+    let binding_path = config.join("bindings.ron");
 
     let game_data = GameDataBuilder::default()
         .with_bundle(TransformBundle::new())?
